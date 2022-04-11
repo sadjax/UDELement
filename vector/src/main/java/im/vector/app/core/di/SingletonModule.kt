b6136f4458b1e16619756f36c5089733aeacabfb
Benoit Marty <benoit@matrix.org>
2022-04-11 15:44:57 +0200

    Create Settings module to add interface for LightweightSettingsStorage

--- vector/src/main/java/im/vector/app/core/di/SingletonModule.kt
+++ vector/src/main/java/im/vector/app/core/di/SingletonModule.kt
@@ -60,6 +60,7 @@ import org.matrix.android.sdk.api.auth.HomeServerHistoryService
 import org.matrix.android.sdk.api.legacy.LegacySessionImporter
 import org.matrix.android.sdk.api.raw.RawService
 import org.matrix.android.sdk.api.session.Session
+import org.matrix.android.sdk.api.settings.LightweightSettingsStorage
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -150,6 +151,11 @@ object VectorStaticModule {
     @Provides
     fun providesRawService(matrix: Matrix): RawService {
         return matrix.rawService()
+    }
+
+    @Provides
+    fun providesLightweightSettingsStorage(matrix: Matrix): LightweightSettingsStorage {
+        return matrix.lightweightSettingsStorage()
     }
 
     @Provides
